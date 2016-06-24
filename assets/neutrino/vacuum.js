3938f8e82da302c94cae03510e869eb3c2d46221
emptymalei <emptymalei@qq.com>
2016-06-24 17:08:20 -0600

    :pill: move legends to the correct place

---
+++ assets/neutrino/vacuum.js
@@ -0,0 +1,11 @@
+// PMNS Matrix
+// The Matrix that mix mass eigenstates into flavor eigenstates
+// pmns3 is define to be an object
+// pmns3 is a real matrix, which is grabbed from wikipedia https://en.wikipedia.org/wiki/Pontecorvo%E2%80%93Maki%E2%80%93Nakagawa%E2%80%93Sakata_matrix
+
+pmns3={e1:0.82, e2:0.54, e3:0.15,
+      m1:0.35, m2:0.70, m3:0.62,
+      t1:0.44, t2:0.45, t3:0.77}
+
+function vacuum3A() {
+}
