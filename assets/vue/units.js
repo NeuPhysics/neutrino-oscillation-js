0120162aff5844c509e6de3dbfd7a35518c1c4c8
OctoMiao <emptymalei@qq.com>
2018-02-11 16:53:20 -0700

    initialize unit conversion

---
+++ assets/vue/units.js
@@ -0,0 +1,21 @@
+function fm2megaev(fm) {
+   return 1/(197*fm)
+}
+
+var units = new Vue({
+  el: '#units',
+  data: {
+    message: 'Unit Conversion',
+    lenght_fm: 1
+   },
+   mounted: function () {
+      // this.execplot();
+      // plotVector();
+   },
+   methods: {
+      
+   },
+   computed: {
+
+   }
+})
